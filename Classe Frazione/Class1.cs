946f3416bc8974b01bcfb170d934a697bbce4af7
riccardo.masserini@icloud.com <riccardo.masserini@icloud.com>
2023-11-02 10:30:10 +0100

    aggiunta dei costruttori e dei controlli per numeratore e denominatore

--- Classe Frazione/Class1.cs
+++ Classe Frazione/Class1.cs
@@ -8,5 +8,45 @@ namespace Classe_Frazione
 {
     internal class Class1
     {
+        class Frazione 
+        {
+            private int numeratore;
+            private int denominatore;
+            public Frazione()
+            {
+                Numeratore = numeratore;
+                Denominatore = denominatore;
+            }
+            public int Numeratore
+            {
+                get { return numeratore; }
+                set
+                {
+                    if(value != 0)
+                    {
+                        numeratore = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("il numeratore non può essere 0");
+                    }
+                }
+            }
+            public int Denominatore
+            {
+                get { return denominatore; }
+                set
+                {
+                    if(value != 0)
+                    {
+                        denominatore = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("il denominatore non può essere 0");     
+                    }
+                }
+            }
+        }
     }
 }
